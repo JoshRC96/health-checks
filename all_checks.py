f45ee24c37b565a4e389c0af7b09a6b4c55894a0
Joshuel Rodriguez <joshuelrodriguezc96@gmail.com>
2023-10-24 09:47:19 -0400

    Updated variable names
    Changed min_absolute to min_gb. Called the disk_usage function using
    the names of the parameters for clarity.

--- all_checks.py
+++ all_checks.py
@@ -11,14 +11,14 @@ def check_reboot():
     else:
         print("\tNo reboot needed.")
 
-def check_disk_usage(disk, min_absolute, min_percent):
+def check_disk_usage(disk, min_gb, min_percent):
     """Return True if there is enough free disk space, false otherwise."""
     du = shutil.disk_usage(disk)
     #Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     #Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_absolute:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return False
     return True
 
@@ -27,7 +27,7 @@ def main():
     #calling the check_reboot function
     check_reboot()
     #Check for at least 10 GB and 20% free
-    if not check_disk_usage("/", 10, 20):
+    if not check_disk_usage(disk="/", min_gb=10, min_percent=20):
         print("\tDisk Usage: ERROR: Not enough space.")
         sys.exit(1)
     else: 
